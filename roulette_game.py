a52fda9797119c1da65ff9ea2c0ba9421eba2243
anton_leonov <an_leonov@yahoo.com>
2020-04-23 00:42:36 +0300

    done

--- roulette_game.py
+++ roulette_game.py
@@ -1,11 +1,43 @@
 import random
 red = [i for i in range(2, 37, 2)]
 black = [j for j in range(1, 37, 2)]
-zero = 0
-# print(red)
-# print(black)
+print("Числа на красном: "+str(red)+". \nЧисла на черном: "+str(black))
+#не знаю как вывести тут без ковычек и запятых в этом случае
 # print(sum(red+black))
 numbers = red+black
 numbers.append(0)
 # print(sorted(numbers))
-
+bet1 = 0
+while bet1 != "цвет" or bet1 != "число":
+    bet1 = input("Выбери на что будешь ставить, число или цвет: ")
+    if bet1 == "цвет":
+        bet2 = input("Выбери цвет, черное или красное: ")
+        num = random.choice(numbers)
+        if bet2 == "черное":
+            for j in black:
+                if num in black:
+                    print("Ты выиграл "+str(num))
+                    break
+                else:
+                    print("Не повезло, выпало: "+str(num))
+                    break
+        else:
+            for i in red:
+                if num in red:
+                    print("Ты выиграл "+str(num))
+                    break
+                else:
+                    print("Не повезло, выпало: "+str(num))
+                    break
+        break
+    elif bet1 == "число":
+        bet2 = int(input("Выбери число от 0 до 36: "))
+        num = random.choice(numbers)
+        if num == bet2:
+            print("Поздравляю ты выиграл!!!")
+            break
+        else:
+            print("Не повезло, выпало: " + str(num))
+            break
+    else:
+        print("Все просто: число или цвет. Давай еще раз")
--- um.py
+++ um.py
@@ -1,12 +1,12 @@
-# for j in range(1, 11):
-#     numbers = []
-#     for i in range(10):
-#         numbers.append(j)
-#     for i in range(1, 11):
-#         numbers[i-1] *= i
-#     print(numbers)
+for j in range(1, 11):
+    numbers = []
+    for i in range(10):
+        numbers.append(j)
+    for i in range(1, 11):
+        numbers[i-1] *= i
+    print("xnj "+str(numbers))
 
 
-numbers = [[value * i for value in range(1,11)] for i in range(1,11)]
-for elem in numbers:
-    print(*elem)
+# numbers = [[j * i for j in range(1,11)] for i in range(1,11)]
+# for elem in numbers:
+#     print(*elem)
