7389e91e39fd09a983c01d8620364b4a5b69d6ef
Anton Leonov <an_leonov@yahoo.com>
2020-04-22 22:15:25 +0300

    roulette1

---
+++ roulette_game.py
@@ -0,0 +1,9 @@
+import random
+red = [i for i in range(2, 37, 2)]
+black = [j for j in range(1, 37, 2)]
+zero = 0
+# print(red)
+# print(black)
+# print(sum(red+black))
+numbers = red+black+zero
+print(sorted(numbers))
