0d90ded00594bf8e7910dca36fc9460b2a806fb9
Anton Leonov <an_leonov@yahoo.com>
2020-04-22 20:54:16 +0300

    tabliza umnizeniya

--- um.py
+++ um.py
@@ -1,4 +1,7 @@
-numbers = []
-for i in range(10):
-    numbers.append(2)
-print(numbers)
+for j in range(1, 11):
+    numbers = []
+    for i in range(10):
+        numbers.append(j)
+    for i in range(1, 11):
+        numbers[i-1] *= i
+    print(numbers)
