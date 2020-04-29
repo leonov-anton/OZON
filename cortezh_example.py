e6f17afbc11146df5fb1bb5abf7b390de4f9a470
Anton Leonov <an_leonov@yahoo.com>
2020-04-29 11:55:45 +0300

    done

--- cortezh_example.py
+++ cortezh_example.py
@@ -1,5 +1,6 @@
-# a = (1, 2, 3)
-# print(type(a))
+a = (1, 2, 3, 5, 8)
+print(type(a))
+print(a)
 
 a = [1, 2, 5, 7]
 print(type(a))
--- dictexample.py
+++ dictexample.py
@@ -1,10 +1,10 @@
 family = {"dad": 60, "mom": 59, "aunt": 50}
 # print(family["dad"])
 family["uncle"] = 55
-# print(family)
+print(type(family))
 
-for key, value in family.items():
-    print("ключ - " + key + ", значение - " + str(value))
-
-print(*list(family.keys()))
-print(*list(family.values()))
+# for key, value in family.items():
+#     print("ключ - " + key + ", значение - " + str(value))
+#
+# print(*list(family.keys()))
+# print(*list(family.values()))
