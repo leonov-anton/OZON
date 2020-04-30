388bccc043b68ec0bdfc0451982879bb11f300de
Anton Leonov <an_leonov@yahoo.com>
2020-04-30 20:06:56 +0300

    final

--- out  vowels.py
+++ out  vowels.py
@@ -1,15 +1,16 @@
-translatedText = ""
+translatedText = '!'
 
 
 def translate(text):
+    global translatedText
     translation_table = str.maketrans("уеаыояию", "        ")
     translated = text.translate(translation_table)
     translated = translated.split()
     translated = "".join(translated)
-    print(translated)
+    translatedText = translated
     return translatedText
 
 
-text = input("Введите текст для перевода: ")
-translate(text)
-print(translatedText)  # не выводится почему-то
+t = input("Введите текст для перевода: ")
+print(translate(t))
+print(translatedText)
