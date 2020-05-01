541055ed18bad638a16d21a8110658d15b1393d7
Anton Leonov <an_leonov@yahoo.com>
2020-05-01 20:17:35 +0300

    final

--- horses_race.py
+++ horses_race.py
@@ -1,5 +1,7 @@
 bet_list = {}
-
+user_bets_kol = ""
+horse_num = ""
+bet = ""
 
 def do_bet(horse_num, bet):
     bet_list.setdefault(horse_num, bet)
@@ -7,8 +9,8 @@ def do_bet(horse_num, bet):
 
 # ставки первого игрока
 user = input("Введите Ваше имя: ")
-user_bats_kol = int(input("Сколько ставок вы хотите сделать: "))
-for i in range(user_bats_kol):
+user_bets_kol = int(input("Сколько ставок вы хотите сделать: "))
+for i in range(user_bets_kol):
     horse_num = int(input("Введите номер лошади, на которую хотите поставить: "))
     bet = int(input("Введите ставку: "))
     if bet > 0:
@@ -24,8 +26,8 @@ user = input("Введите Ваше имя: ")
 print("Внимание! Ставки на лошадей под номерами ", end = '')
 print(*bet_list.keys(), end = '')
 print(" уже сделаны и приняты не будут.")
-user_bats_kol = int(input("Сколько ставок вы хотите сделать: "))
-for i in range(user_bats_kol):
+user_bets_kol = int(input("Сколько ставок вы хотите сделать: "))
+for i in range(user_bets_kol):
     horse_num = int(input("Введите номер лошади, на которую хотите поставить: "))
     bet = int(input("Введите ставку: "))
     if bet > 0:
