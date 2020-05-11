fa38559d0d05c43a4f0a1cb4bafd12531a51aefa
Anton Leonov <an_leonov@yahoo.com>
2020-05-11 19:59:00 +0300

    finnal

--- DZ-8/DZ-8.py
+++ DZ-8/DZ-8.py
@@ -1,15 +1,14 @@
 import re
 
-email = input("Введите Вашу почту: ")
-login = input("Введите Ваш логин: ")
-
-users_list = open('users_list.txt', 'a', encoding='utf-8')
-
-if re.match(r'^[a-zA-Z0-9_\.\-]+@\w+\.\w+$', email) and re.match(r'^[a-zA-Z0-9_\.\-\@]+$', login):
-    users_list.write(email + ', ' + login + '\n')
-elif re.match(r'^[a-zA-Z0-9_\.\-]+@\w+\.\w+$', email) is None:
-    print("Вы ввели не почту")
-else:
-    print("Вы ввели недопустимый логин")
-
- users_list.close()
+while True:
+    email = input("Введите Вашу почту: ")
+    login = input("Введите Ваш логин: ")
+    if re.match(r'^[a-zA-Z0-9_\.\-]+@\w+\.\w+$', email) and re.match(r'^[a-zA-Z0-9_\.\-\@]+$', login):
+        users_list = open('users_list.txt', 'a', encoding='utf-8')
+        users_list.write(email + ', ' + login + '\n')
+        users_list.close()
+        exit()
+    elif re.match(r'^[a-zA-Z0-9_\.\-]+@\w+\.\w+$', email) is None:
+        print("Вы ввели не почту, введите данные заново")
+    else:
+        print("Вы ввели недопустимый логин, введите данные заново")
