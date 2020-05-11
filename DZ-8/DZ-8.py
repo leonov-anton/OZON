2d5cba94a62d2c4c037291957abc2d393ee27833
Anton Leonov <an_leonov@yahoo.com>
2020-05-11 20:01:22 +0300

    finnal

--- DZ-8/DZ-8.py
+++ DZ-8/DZ-8.py
@@ -7,7 +7,7 @@ while True:
         users_list = open('users_list.txt', 'a', encoding='utf-8')
         users_list.write(email + ', ' + login + '\n')
         users_list.close()
-        exit()
+        break
     elif re.match(r'^[a-zA-Z0-9_\.\-]+@\w+\.\w+$', email) is None:
         print("Вы ввели не почту, введите данные заново")
     else:
