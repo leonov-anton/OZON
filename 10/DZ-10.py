701eeb164aac2c9f14d829cfaa30680adb1d26f2
Anton Leonov <an_leonov@yahoo.com>
2020-05-18 19:02:54 +0300

    finnal

--- 10/DZ-10.py
+++ 10/DZ-10.py
@@ -1,24 +1,25 @@
 from simple_benchmark import benchmark
 import matplotlib.pyplot as plt
 
-def cicle_list(n):
-    for i in range(2, 6):
+def cicle_list(n, m):
+    for i in range(2, m+1):
         a.append(n ** i)
     return a
 
 
-def list_hrec(n):
-    return list_iter(2, 5, n)
+def list_hrec(m, n):
+    return list_iter(2, m, n)
 
 
-def list_iter(iter, limit, n):
-    if iter > limit:
+def list_iter(iter, m, n):
+    if iter > m:
         return b
     b.append(n ** iter)
-    return list_iter(iter + 1, limit, n)
+    return list_iter(iter + 1, m, n)
 
 
 n = int(input("Введите число: "))
+m = int(input("Введите максимальную степень: "))
 a = []
 b = []
 
