85483b6145e5d19a001fbf8ea728d9c37b63e78d
Anton Leonov <an_leonov@yahoo.com>
2020-05-25 16:25:18 +0300

    Final

--- 12/DZ-12/Calc.py
+++ 12/DZ-12/Calc.py
@@ -17,9 +17,9 @@ class Calculator:
         """Addition x+y"""
         file = open('operation_log.json', 'a', encoding='utf-8')
         if self.verif_numb(self.first) and self.verif_numb(self.second):
-                json.dump(f"{float(self.first)} + {float(self.second)} = "
-                          f"{float(self.first) + float(self.second)}", file)
-                file.close()
+            json.dump(f"{float(self.first)} + {float(self.second)} = "
+                      f"{float(self.first) + float(self.second)}", file)
+            file.close()
 
     def difference(self):
         """Difference x-y"""
@@ -72,6 +72,6 @@ class Calculator:
 
     def verif_numb(self, number):
         """Verification value as s number"""
-        if re.match("^(-)*[0-9]+(\.)*[0-9]*", number):
+        if re.match(r"^(-)*[0-9]+(\.)*[0-9]*", number):
             return True
         return False
--- 12/DZ-12/calc_work.py
+++ 12/DZ-12/calc_work.py
@@ -4,3 +4,5 @@ x = input("Введите первое число (X): ")
 y = input("Введите второе число (Y): ")
 
 numbers = Calculator(x, y)
+
+numbers.addition()
