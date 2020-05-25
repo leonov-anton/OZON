cd3b59dca94bed088ed43d329cd55f6ca0f39d1f
Anton Leonov <an_leonov@yahoo.com>
2020-05-25 19:14:35 +0300

    Final

--- 12/DZ-12/CalcTesting.py
+++ 12/DZ-12/CalcTesting.py
@@ -8,18 +8,16 @@ class CalculatorTesting(unittest.TestCase):
     """
 
     def setUp(self):
-        x = 1
-        y = 1
-        self.my_calculation = Calculator(x, y)
+        self.my_calculation = Calculator(1, 1)
 
     def number_input_testing(self):
         self.assertEqual(self.my_calculation.addition(), 2)
 
-    def text_input_testing(self):
-        self.assertFalse(self.my_calculation.verif_numb("sfsd"))
-
-    def epmty_input_testing(self):
-        self.assertFalse(self.my_calculation.verif_numb(""))
+    # def text_input_testing(self):
+    #     self.assertFalse(self.my_calculation.verif_numb("sfsd"))
+    #
+    # def epmty_input_testing(self):
+    #     self.assertFalse(self.my_calculation.verif_numb(""))
 
 
 unittest.main()
