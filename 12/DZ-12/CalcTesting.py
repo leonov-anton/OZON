4296d413c328e7d0f27e7bb61835079fc98c0bf1
Anton Leonov <an_leonov@yahoo.com>
2020-05-25 16:23:48 +0300

    Final

--- 12/DZ-12/CalcTesting.py
+++ 12/DZ-12/CalcTesting.py
@@ -7,16 +7,22 @@ class CalculatorTesting(unittest.TestCase):
     Tests for Calculator
     """
 
+    def setUp(self):
+        x = 5
+        y = 3
+        self.my_calculation = Calculator(x, y)
+
     def number_input_testing(self):
-        self.x = Calculator.verif_numb('5')
-        self.assertEqual(self.x, True)
+        self.my_calculation.verif_numb('5')
+        self.assertEqual(self.my_calculation.verif_numb('5'), True)
 
     def text_input_testing(self):
-        self.x = Calculator.verif_numb('safdsf')
-        self.assertFalse(self.x)
+        self.my_calculation.verif_numb('safdsf')
+        self.assertFalse(self.my_calculation.verif_numb('safdsf'))
 
     def epmty_input_testing(self):
-        self.x = Calculator.verif_numb('')
-        self.assertFalse(self.x)
+        self.my_calculation.verif_numb('')
+        self.assertFalse(self.my_calculation.verif_numb(''))
+
 
 unittest.main()
