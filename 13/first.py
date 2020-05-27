7bcfcec10d0a68e53a1f96bc0ce81fe056a020c5
Anton Leonov <an_leonov@yahoo.com>
2020-05-27 20:55:11 +0300

    Final

---
+++ 13/first.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QPushButton
+
+# Создание приложения
+app = QApplication(sys.argv)
+
+# Создание окна
+window = QWidget()
+
+window.setGeometry(100, 100, 100, 100)
+window.move(60, 15)
+msg = QLabel('<h1>Hello!</h1>', parent=window)
+msq.move(60, 15)
+
+window.move(60, 50)
+ok_button = QPushButton("OK", parent=window)
+ok_button.move(60, 50)
+
+window.show()
+sys.exit(app.exec())
