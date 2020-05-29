08fbcbbc07e37ee769a3c15d57c563a9e5978c73
Anton Leonov <an_leonov@yahoo.com>
2020-05-29 14:01:35 +0300

    Final

--- Calculator/Calc_GUI.py
+++ Calculator/Calc_GUI.py
@@ -1,9 +1,30 @@
 import sys
-from calul_pyqt import *
+from Calcul import Calculator
+from calculpyqt import *
+
+class myCalc(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(self.add)
+
+    def add(self):
+        x = float(self.ui.lineEdit.text())
+        y = float(self.ui.lineEdit2.text())
+        sum = x + y
+        self.ui.label.setText(str(sum))
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    myapp = exemple()
+    myapp = myCalc()
     myapp.show()
     sys.exit(app.exec_())
 
